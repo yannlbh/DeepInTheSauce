b861e361bbf957700ba3789c43dbedf6dc397a06
Bastien-crg <bastien.corgnac@gmail.com>
2024-05-15 08:10:44 +0200

    enhanced

--- multi_layer.py
+++ multi_layer.py
@@ -28,13 +28,8 @@ y_test = keras.utils.to_categorical(y_test, num_classes)
 
 model = keras.Sequential(
     [
-        keras.Input(shape=input_shape),
-        layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
-        layers.MaxPooling2D(pool_size=(2, 2)),
-        layers.Flatten(),
-        layers.Dropout(0.5),
+        layers.Flatten(input_shape=(28,28)),
+        layers.Dense(10, activation="relu"),
         layers.Dense(num_classes, activation="softmax"),
     ]
 )
@@ -42,7 +37,7 @@ model = keras.Sequential(
 model.summary()
 
 batch_size = 128
-epochs = 15
+epochs = 1
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
